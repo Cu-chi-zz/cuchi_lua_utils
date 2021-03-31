f50f2f608894d9e0159ecced6d3aa0fbb0689456
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-03-31 19:00:19 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cuchi_lua_utils
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose a key to modify:");
+            string keyChoosed = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(keyChoosed))
+            {
+                Console.WriteLine("Please, choose a key!");
+                Console.ReadKey();
+                Main();
+                return;
+            }
+        }
+    }
+}
