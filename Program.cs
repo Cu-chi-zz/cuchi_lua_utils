66992a8ef8acdba4308e4eb98af436c6805af10d
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-03-31 19:11:33 +0200

    improving check

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace cuchi_lua_utils
         private static void Main()
         {
             Console.Clear();
-            Console.WriteLine("Choose a key to modify:");
+            Console.WriteLine("Choose a key to modify: events/vars");
             string keyChoosed = Console.ReadLine();
 
-            if (string.IsNullOrEmpty(keyChoosed))
+            if (string.IsNullOrEmpty(keyChoosed) || keyChoosed != "events" && keyChoosed != "vars")
             {
-                Console.WriteLine("Please, choose a key!");
+                Console.WriteLine("Please, choose a good key! Press a key to reset.");
                 Console.ReadKey();
                 Main();
                 return;
