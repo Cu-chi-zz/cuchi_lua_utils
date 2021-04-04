c77108550b7caa814fb512188758298a15cd00bc
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-04-04 13:07:43 +0200

    add(copy directory: only lua files) fix(events with " and ') new(end message) new(enable or disable logs) remove(choose between events or vars) add(counter of lines) new(system of backups)

--- Program.cs
+++ Program.cs
@@ -10,22 +10,36 @@ namespace cuchi_lua_utils
         private static void Main()
         {
             Console.Clear();
-            Console.WriteLine("Choose a key to modify: events/vars");
-            string keyChoosed = Console.ReadLine();
+            Console.ResetColor();
 
-            if (string.IsNullOrEmpty(keyChoosed) || keyChoosed != "events" && keyChoosed != "vars")
-            {
-                OnBadArgument("Please, choose a good key! Press a key to reset.");
-                return;
-            }
+            Console.WriteLine("Welcome!\n\n");
 
             Console.WriteLine("Specify a path");
+            Console.ForegroundColor = ConsoleColor.Cyan;
             string pathEntered = Console.ReadLine();
+            Console.ResetColor();
 
             if (!Directory.Exists(pathEntered))
             {
-                OnBadArgument("Please, choose a working path! Press a key to reset.");
+                OnBadArgument("Please, choose a working path! Press a key to retry.");
                 return;
+            }
+
+            bool logs = false;
+            Console.WriteLine("Enable logs: y/n");
+            string enableLogs = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(enableLogs) || enableLogs != "y" && enableLogs != "n")
+            {
+                OnBadArgument("Please, choose yes (y) or no (n)! Press a key to retry.");
+                return;
+            }
+            else
+            {
+                if (enableLogs == "y")
+                {
+                    logs = true;
+                }
             }
 
             try
@@ -33,32 +47,59 @@ namespace cuchi_lua_utils
                 Stopwatch watchFolder = new Stopwatch();
                 watchFolder.Start();
 
+                if (!logs)
+                {
+                    Console.WriteLine("Processing...");
+                }
+
                 int filesCounter = 0;
+                int linesCounter = 0;
+                int totalLinesSkipped = 0;
+                int totalLinesReplaced = 0;
+
+                bool directoryExist = true;
+                int iDir = 1;
+                while (directoryExist)
+                {
+                    if (!Directory.Exists($@".\backups-{iDir}\"))
+                    {
+                        Directory.CreateDirectory($@".\backups-{iDir}\");
+                        directoryExist = false;
+                        break;
+                    }
+                    iDir++;
+                }
+
+                if (logs)
+                {
+                    Stopwatch watchSave = new Stopwatch();
+                    watchSave.Start();
+                    var dirInfos = new DirectoryInfo(pathEntered);
+                    Console.WriteLine($"Saving {dirInfos.Name} to backups-{iDir}...");
+                    DirectoryCopy(pathEntered, $@".\backups-{iDir}\", true);
+                    watchSave.Stop();
+                    Console.WriteLine($"Saved to backups-{iDir}: took {Math.Round(watchSave.Elapsed.TotalSeconds, 2)} seconds");
+                }
+                else
+                {
+                    DirectoryCopy(pathEntered, $@".\backups-{iDir}\", true);
+                }
+
                 foreach (string file in Directory.GetFiles(pathEntered, "*.lua", SearchOption.AllDirectories))
                 {
-                    if (!Directory.Exists(@".\backups\"))
-                    {
-                        Directory.CreateDirectory(@".\backups\");
-                    }
-
                     var currentFile = new FileInfo(file);
 
-                    if (!File.Exists($@".\backups\{currentFile.Name}"))
+                    if (logs)
                     {
-                        File.Copy(file, $@".\backups\{currentFile.Name}");
-                    }
-                    else
-                    {
-                        File.Delete($@".\backups\{currentFile.Name}");
-                        File.Copy(file, $@".\backups\{currentFile.Name}");
+                        Console.WriteLine($"File: {currentFile.Name}");
                     }
 
-                    Console.WriteLine($"File: {currentFile.Name}");
                     Stopwatch watchFile = new Stopwatch();
                     watchFile.Start();
                     string[] lines = File.ReadAllLines(file);
                     if (lines.Length > 0)
                     {
+                        linesCounter += lines.Length;
                         using (StreamWriter f = new StreamWriter($@"{file}"))
                         {
                             for (int currLine = 0; currLine < lines.Length; currLine++)
@@ -76,18 +117,40 @@ namespace cuchi_lua_utils
                                         {
                                             linesSp = lines[currLine].Split('\'');
                                         }
+                                        else
+                                        {
+                                            if (FindFirstCharContainsString(lines[currLine]) == "\"")
+                                            {
+                                                linesSp = lines[currLine].Split('"');
+                                            }
+                                            else if (FindFirstCharContainsString(lines[currLine]) == "\'")
+                                            {
+                                                linesSp = lines[currLine].Split('\'');
+                                            }
+                                            else if (FindFirstCharContainsString(lines[currLine]) == "unfinded" && logs)
+                                            {
+                                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                                Console.WriteLine($"{currLine}: skipped event. ({lines[currLine]})");
+                                                Console.ResetColor();
+                                            }
+                                        }
                                         // Erreur causé : si les deux, aucun n'est trouvé
 
                                         try
                                         {
                                             string nameOfEvent = linesSp[1];
                                             lines[currLine] = lines[currLine].Replace(nameOfEvent, Base64Encode(nameOfEvent));
+                                            totalLinesReplaced++;
                                         }
-                                        catch (IndexOutOfRangeException ex)
+                                        catch (IndexOutOfRangeException)
                                         {
-                                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                            Console.WriteLine($"{currLine}: skipped event. ({lines[currLine]})");
-                                            Console.ResetColor();
+                                            totalLinesSkipped++;
+                                            if (logs)
+                                            {
+                                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                                Console.WriteLine($"{currLine}: skipped event (out of range). ({lines[currLine]})");
+                                                Console.ResetColor();
+                                            }
                                         }
                                         catch (Exception ex)
                                         {
@@ -97,39 +160,47 @@ namespace cuchi_lua_utils
                                     }
                                 }
                             }
-
                             filesCounter++;
                         }
 
                         File.WriteAllLines(file, lines);
                         watchFile.Stop();
-                        if (watchFile.Elapsed.TotalMilliseconds > 1000)
+                        if (logs)
                         {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {watchFile.Elapsed.TotalSeconds} seconds");
-                            Console.ResetColor();
-                        }
-                        else if (watchFile.Elapsed.TotalMilliseconds > 5000)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {watchFile.Elapsed.TotalSeconds} seconds");
-                            Console.ResetColor();
-                        }
-                        else if (watchFile.Elapsed.TotalMilliseconds < 200)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {watchFile.Elapsed.TotalSeconds} seconds");
-                            Console.ResetColor();
-                        }
-                        else
-                        {
-                            Console.ResetColor();
-                            Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {watchFile.Elapsed.TotalSeconds} seconds");
+                            if (watchFile.Elapsed.TotalMilliseconds > 1000)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {Math.Round(watchFile.Elapsed.TotalSeconds, 5)} seconds");
+                                Console.ResetColor();
+                            }
+                            else if (watchFile.Elapsed.TotalMilliseconds > 5000)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {Math.Round(watchFile.Elapsed.TotalSeconds, 5)} seconds");
+                                Console.ResetColor();
+                            }
+                            else if (watchFile.Elapsed.TotalMilliseconds < 200)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {Math.Round(watchFile.Elapsed.TotalSeconds, 5)} seconds");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.ResetColor();
+                                Console.WriteLine($"Replaced for {currentFile.Name}\nProcess (replaced {lines.Length} lines) took: {Math.Round(watchFile.Elapsed.TotalSeconds, 5)} seconds");
+                            }
                         }
                     }
                 }
                 watchFolder.Stop();
-                Console.WriteLine($"Process (all files: {filesCounter}) took: {watchFolder.Elapsed.TotalSeconds} seconds");
+                Console.WriteLine($"--------------------------------------------------------------");
+                Console.WriteLine($"Process took: {Math.Round(watchFolder.Elapsed.TotalSeconds, 2)} seconds");
+                Console.WriteLine($"Total files: {filesCounter}");
+                Console.WriteLine($"Total lines: {linesCounter}");
+                Console.WriteLine($"Total lines skipped: {totalLinesSkipped}");
+                Console.WriteLine($"Total lines replaced: {totalLinesReplaced}");
+                Console.WriteLine($"--------------------------------------------------------------");
             }
             catch (UnauthorizedAccessException ex)
             {
@@ -154,6 +225,62 @@ namespace cuchi_lua_utils
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string FindFirstCharContainsString(string line)
+        {
+            string def = "unfinded";
+            foreach (char c in line)
+            {
+                if (c == '"')
+                {
+                    return c.ToString();
+                }
+                else if (c == '\'')
+                {
+                    return c.ToString();
+                }
+            }
+            return def;
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (file.Extension == ".lua")
+                {
+                    string tempPath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(tempPath, false);
+                }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
         }
     }
 }
